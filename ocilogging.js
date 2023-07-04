c64f820a4b3a5b2de103e92dbbd3fd2b16c39ceb
Anand <anand6@buffalo.edu>
2023-07-04 15:45:23 -0700

    Update ocilogging.js
    Update ocilogging.js

--- ocilogging.js
+++ ocilogging.js
@@ -15,7 +15,7 @@ const provider = new common.ConfigFileAuthenticationDetailsProvider(
   try {
     // Create a service client
     const client = new loganalytics.LogAnalyticsClient({ authenticationDetailsProvider: provider });
-
+    var bodytext = Buffer.from("ExampleStreamValue", 'utf8');
     // Create a request and dependent object(s).
     const uploadLogFileRequest = {
       namespaceName: "omcinternal",
@@ -24,11 +24,12 @@ const provider = new common.ConfigFileAuthenticationDetailsProvider(
       filename: "Node.log",
       opcMetaLoggrpid: "ocid1.loganalyticsloggroup.oc1.iad.amaaaaaae5xv5jia25z5hocr7gtkep3i7ifxfrqw6lrmfzqnt7tdbhnuanaq",
       // Create a Stream, for example, by calling a helper function like below.
-      uploadLogFileBody: generateStreamFromString('ExampleStreamValue'),
+      uploadLogFileBody: bodytext,
     };
 
     // Send request to the Client.
     const uploadLogFileResponse = await client.uploadLogFile(uploadLogFileRequest);
+    console.log("Upload Log File Response "+ JSON.stringify(uploadLogFileResponse));
   } catch (error) {
     console.log("uploadLogFile Failed with error  " + JSON.stringify(error));
   }
