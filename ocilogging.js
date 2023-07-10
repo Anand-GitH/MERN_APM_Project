17425ee434b8af35c01dedafa389f78136c3f625
Anand <anand6@buffalo.edu>
2023-07-09 19:14:02 -0700

    Log changes
    Log changes

--- ocilogging.js
+++ ocilogging.js
@@ -2,8 +2,22 @@
 "use strict";
 const loganalytics = require("oci-loganalytics");
 const common = require("oci-common");
+const log4js = require('log4js');
+var bunyan = require('bunyan');
 
-// Using personal configuration
+function generateStreamFromString(data) {
+  let Readable = require("stream").Readable;
+  let stream = new Readable();
+  stream.push(data);
+  stream.push(null);
+  return stream;
+}
+
+function ocilogging(data){
+
+//initialization of the OCI logging parameters
+//config contains API key and tenancy, user details & profile to use
+
 const configurationFilePath = "~/.oci/config";
 const configProfile = "DEFAULT";
 const provider = new common.ConfigFileAuthenticationDetailsProvider(
@@ -11,34 +25,60 @@ const provider = new common.ConfigFileAuthenticationDetailsProvider(
   configProfile
 );
 
+//Using node js OCI logging analytics client to send log message to the LA
 (async () => {
   try {
     // Create a service client
     const client = new loganalytics.LogAnalyticsClient({ authenticationDetailsProvider: provider });
-    var bodytext = Buffer.from("ExampleStreamValue", 'utf8');
+    var bodytext = Buffer.from(data, 'utf8');
     // Create a request and dependent object(s).
     const uploadLogFileRequest = {
       namespaceName: "omcinternal",
       uploadName: "Test_anand",
-      logSourceName: "MernApp",  #log source and then parser and use it in the code 
-      filename: "Node.log", #remove this 
+      logSourceName: "MernApp",  
+      filename: "Node.log",
       opcMetaLoggrpid: "ocid1.loganalyticsloggroup.oc1.iad.amaaaaaae5xv5jia25z5hocr7gtkep3i7ifxfrqw6lrmfzqnt7tdbhnuanaq",
-      // Create a Stream, for example, by calling a helper function like below.
       uploadLogFileBody: bodytext,
     };
 
     // Send request to the Client.
-    const uploadLogFileResponse = await client.uploadLogFile(uploadLogFileRequest);
-    console.log("Upload Log File Response "+ JSON.stringify(uploadLogFileResponse));
+    const uploadLogFileResponse = client.uploadLogFile(uploadLogFileRequest);
   } catch (error) {
     console.log("uploadLogFile Failed with error  " + JSON.stringify(error));
   }
-})();
-
-function generateStreamFromString(data) {
-  let Readable = require("stream").Readable;
-  let stream = new Readable();
-  stream.push(data); // the string you want
-  stream.push(null);
-  return stream;
+})()
 }
+
+function OCIInfoStream() {}
+
+OCIInfoStream.prototype.write = function(data) {
+  ocilogging(data)
+}
+
+function DebugInfoStream() {}
+
+DebugInfoStream.prototype.write = function(data) {
+  ocilogging(data)
+}
+
+function getLogger(){
+  
+  var log = bunyan.createLogger({
+    name: "OCILogger",
+    streams: [{
+      level: "info",
+      stream: new OCIInfoStream()
+    }, {
+      level: "debug",
+      stream: new DebugInfoStream()
+    }]
+  })
+
+  return log;
+
+}
+
+var log=getLogger()
+log.level(bunyan.DEBUG)
+log.debug("debug1 anand me")
+log.debug("debug2 anand me")
